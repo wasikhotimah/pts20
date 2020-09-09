ba3b487f1f818431f2b82d8006480656fb5bc124
wasikhotimah <70928372+wasikhotimah@users.noreply.github.com>
2020-09-09 20:58:22 +0700

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,16 @@
+package ujian;
+
+public class string1 {
+
+  public static void main(String[] args) {
+   String myString1 ="abcdefghijklmnopqrstuvwxyz";
+   
+   
+   System.out.println(myString1.charAt(22));
+   System.out.println(myString1.charAt(0));
+   System.out.println(myString1.charAt(18));
+   System.out.println(myString1.charAt(8));
+   System.out.println(myString1.charAt(10));
+   System.out.println(myString1.charAt(7));
+  }
+}
